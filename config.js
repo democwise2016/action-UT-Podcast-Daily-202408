334e433486d6b9359c7418d95a874d6dfaac5261
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-08-02 20:33:31 +0800

    西元2024年08月02日 (週五) 20時33分31秒 CST

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const ConfigCheck = require('./app/config/ConfigCheck.js')
 const feedList = require('./config-list.js')
 
 let CONFIG = {
-  publicURL: `https://democwise2016.github.io/action-UT-Podcast/`,
-  publicURLShorten: `https://ppt.cc/fMYRpx`,
-  thumbnailBorderColor: '2980b9',
+  publicURL: `https://democwise2016.github.io/action-UT-Podcast-Daily-202408/`,
+  publicURLShorten: `https://rb.gy/h30k3f`,
+  thumbnailBorderColor: 'E85D75',
   maxDownloadItemPerFeed: 5,
   maxDownloadFeed: 30,
   newArrialMax: 5,
